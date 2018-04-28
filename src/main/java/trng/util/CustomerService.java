da513960bc5891e24e1943174f746bcccbc7e1ff
jtrng <jtrng4u@gmail.com>
2018-04-28 11:54:27 -0500

    Added....

--- src/main/java/trng/util/CustomerService.java
+++ src/main/java/trng/util/CustomerService.java
@@ -3,7 +3,15 @@ package trng.util;
 import trng.jaxb.customer.Customer;
 
 public class CustomerService {
+	public boolean doMore(Customer customer) {
+		return customer.getSalary() > 1000;
+	}
+	
 	public boolean isElegible(Customer customer) {
+		return customer.getSalary() > 1000;
+	}
+	
+	public boolean doOther(Customer customer) {
 		return customer.getSalary() > 1000;
 	}
 }
